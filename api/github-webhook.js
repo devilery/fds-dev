064ef77e25f6a236f094742a1046a5336ea773d8
Predrag Ristic <predyristic@gmail.com>
2019-11-30 20:48:52 +0100

    commit deploy

--- api/github-webhook.js
+++ api/github-webhook.js
@@ -1,4 +1,17 @@
 module.exports = async (req, res) => {
-  const { body } = req.body
-  res.end('github')
+  const { body } = req
+  
+  console.log(body)
+  switch (true) {
+    case !!body.pull_request:
+      console.log('pull request')
+      console.log(body.pull_request)
+      console.log(body)
+      break;
+    default:
+      console.log('untracked event')
+      break;
+  }
+  res.status(200).end()
 }
+
