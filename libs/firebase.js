357ce728e53d870bec4322bcf59cdcf2fe65a194
Predrag Ristic <predyristic@gmail.com>
2019-11-30 23:40:33 +0100

    now

--- libs/firebase.js
+++ libs/firebase.js
@@ -2,7 +2,7 @@ var admin = require('firebase-admin');
 
 module.exports = function() {
   const app = admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_AUTH_JSON)),
+    credential: admin.credential.cert(JSON.parse(process.env.firebase_auth_json)),
     databaseURL: "https://fds-dev-260620.firebaseio.com"
   });
   return app
