42f7d703b09a6f7398a193b682c3790c616baa73
Predrag Ristic <predyristic@gmail.com>
2019-11-30 23:04:03 +0100

    firebase

--- api/github-webhook.js
+++ api/github-webhook.js
@@ -6,8 +6,6 @@ module.exports = async (req, res) => {
   const { body } = req
   const eventName = req.headers['x-github-event']
   
-  console.log(firebase.database())
-
   switch (eventName) {
     case 'pull_request':
       processGithubPullRequest(body)
--- libs/firebase.js
+++ libs/firebase.js
@@ -2,8 +2,9 @@ var admin = require('firebase-admin');
 
 module.exports = function() {
   const app = admin.initializeApp({
-    credential: admin.credential.cert(process.env.FIREBASE_AUTH_JSON),
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_AUTH_JSON)),
     databaseURL: "https://fds-dev-260620.firebaseio.com"
   });
   return app
-}
+}()
+
