935a995693c347cf9513bef49a8fa0c8054cf968
Tomas Ruzicka <me@tomasr.com>
2019-11-30 21:49:28 +0100

    ci status

--- scripts/ci_webhook.sh
+++ scripts/ci_webhook.sh
@@ -3,11 +3,12 @@
 URL=https://fds-dev.now.sh/api/event-webhook
 
 ci_webhook() {
+	# $1 = event type; $2 = any extra JSON string
 	echo Sending webhook ...
 	# Send all CIRCLE* env vars
 	curl -X POST "$URL" \
 	  -H "Content-Type: application/json" \
-	  -d"{`env | grep CIRCLE | sed 's/=/":"/' | sed 's/\(.*\)/"\1",/'` \"type\":\"$1\"}" #  pipeline.finished
+	  -d"{`env | grep CIRCLE | sed 's/=/":"/' | sed 's/\(.*\)/"\1",/'` \"type\":\"$1\"$2}" #  pipeline.finished
 }
 
-[ ! -z "$1" ] && ci_webhook "$1"
+[ ! -z "$1" ] && ci_webhook "$1" "$2"
