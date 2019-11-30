12ddd1961810de94fd3cf78cfe4e543f7664c627
Predrag Ristic <predyristic@gmail.com>
2019-11-30 20:51:03 +0100

    update

--- api/github-webhook.js
+++ api/github-webhook.js
@@ -1,12 +1,10 @@
 module.exports = async (req, res) => {
   const { body } = req
+  const eventName = req.get('X-GitHub-Event')
   
-  console.log(body)
-  switch (true) {
-    case !!body.pull_request:
+  switch (eventName) {
+    case 'pull_request':
       console.log('pull request')
-      console.log(body.pull_request)
-      console.log(body)
       break;
     default:
       console.log('untracked event')
