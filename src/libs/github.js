20aa58e8158c97c9061cf1195fd08ee109e99209
Predrag Ristic <predyristic@gmail.com>
2019-12-01 12:23:43 +0100

    add github data

--- src/libs/github.js
+++ src/libs/github.js
@@ -1,4 +1,5 @@
-const { emmit } = require('./event.js')
+const { emmit } = require('./event.js');
+const { getPullRequestsForCommit } = require('./github-api');
 
 
 function processGithubPullRequest(pullRequestEvent) {
@@ -14,6 +15,15 @@ function processGithubPullRequest(pullRequestEvent) {
     website_url: githubPullRequest.html_url,
     title: githubPullRequest.title,
     user_id: userId,
+    repository: {
+      id: pullRequestEvent.repository.id,
+      name: pullRequestEvent.repository.name,
+      owner: {
+        id: pullRequestEvent.repository.owner.id,
+        login: pullRequestEvent.repository.owner.login
+      },
+      raw_data: pullRequestEvent.repository
+    },
     raw_data: pullRequestEvent
   }
 
@@ -34,10 +44,30 @@ function processCommitStatus(statusEvent) {
   const action = pullRequestEvent.action
   const sender = pullRequestEvent.sender
   const githubCommitStatus = pullRequestEvent.status
+
+  const StatusData = {
+    status: githubCommitStatus.status,
+    from: 'github',
+    id: githubCommitStatus.id,
+    commit_sha: githubCommitStatus.sha,
+    name: githubCommitStatus.name,
+    target_url: githubCommitStatus.target_url,
+    context: githubCommitStatus.context,
+    description: githubCommitStatus.description,
+    raw_data: pullRequestEvent
+  }
+
+  let commitPullRequests = await getPullRequestsForCommit(githubCommitStatus.repository.owner.login, githubCommitStatus.repository.name, githubCommitStatus.sha);
+
+  
 }
 
-function findUserIdByGithubId(githubEventUser) {
+async function findUserIdByGithubId(githubEventUser) {
   return 'RANDOM_ID' + Math.random().toString()
+}
+
+async function findPullRequestsById(repo, ids) {
+  
 }
 
 module.exports = {
