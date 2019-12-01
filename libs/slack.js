d9e991b2d112c14d7ecdc8d1af7ad285eabf4c37
Marek Vybiral <marekvybiral@email.cz>
2019-12-01 01:31:50 +0100

    add secret to code

--- libs/slack.js
+++ libs/slack.js
@@ -7,8 +7,10 @@ async function sendMessage(reqData) {
 
 	const res = await request.post('https://slack.com/api/chat.postMessage')
 		.set('Content-Type', 'application/json;charset=utf-8')
-		.set('Authorization', 'Bearer xoxb-7093049764-856934218934-oG3OvZA754e09e0lBSvcYDKY')
-		.send(JSON.stringify(rqeData))
+		.set('Authorization', 'Bearer xoxb-7093049764-856934218934-mNrIjU24BFGpNTQAFK3dzvYJ')
+		.send(JSON.stringify(reqData))
+
+	console.log(res);
 }
 
 module.exports = {
