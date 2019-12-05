fae8fb744fd6d9d985576b3f5f84a76e6fbe45f9
Marek Vybiral <marekvybiral@email.cz>
2019-12-05 01:17:11 +0100

    remove rotation

--- src/libs/slack-api.js
+++ src/libs/slack-api.js
@@ -20,41 +20,48 @@ async function getAuthInfo(code, redirectUri=true) {
 
 
 async function get(url, options) {
+	console.log(url)
+	console.log(options)
 	var res = await axios.get(url, options)
-	if (['invalid_auth', 'not_authed'].includes(res.data.error)) {
-		const teamsRefs = await firestore.collection('teams').get()
+	console.log(res)
+	// if (['invalid_auth', 'not_authed'].includes(res.data.error)) {
+	// 	const teamsRefs = await firestore.collection('teams').get()
 
-		let authTeam = null
-		teamsRefs.docs.map(doc => doc.data()).filter(async team => {
-			if (team.slack_bot_access_token == options.headers.Authorization.replace('Bearer ', ''))
-				authTeam = team
-		})
-		authInfo = await getAuthInfo(authTeam.slack_auth_code)
-		const teamRef = await firestore.collection('teams').doc(team.id).get()
-		await teamRef.update({slack_bot_access_token: authInfo.botAccessToken})
-		options.headers.Authorization = `Bearer ${authInfo.botAccessToken}`
-		res = await axios.get(url, options)
-	}
+	// 	let authTeam = null
+	// 	teamsRefs.docs.map(doc => doc.data()).filter(async team => {
+	// 		if (team.slack_bot_access_token == options.headers.Authorization.replace('Bearer ', ''))
+	// 			authTeam = team
+	// 	})
+	// 	authInfo = await getAuthInfo(authTeam.slack_auth_code)
+	// 	const teamRef = await firestore.collection('teams').doc(team.id).get()
+	// 	await teamRef.update({slack_bot_access_token: authInfo.botAccessToken})
+	// 	options.headers.Authorization = `Bearer ${authInfo.botAccessToken}`
+	// 	res = await axios.get(url, options)
+	// }
 	return res
 }
 
 
 async function post(url, data, options) {
+	console.log(url)
+	console.log(data)
+	console.log(options)
 	var res = await axios.get(url, data, options)
-	if (['invalid_auth', 'not_authed'].includes(res.data.error)) {
-		const teamsRefs = await firestore.collection('teams').get()
+	console.log(res)
+	// if (['invalid_auth', 'not_authed'].includes(res.data.error)) {
+	// 	const teamsRefs = await firestore.collection('teams').get()
 
-		let authTeam = null
-		teamsRefs.docs.map(doc => doc.data()).filter(async team => {
-			if (team.slack_bot_access_token == options.headers.Authorization.replace('Bearer ', ''))
-				authTeam = team
-		})
-		authInfo = await getAuthInfo(authTeam.slack_auth_code)
-		const teamRef = await firestore.collection('teams').doc(team.id).get()
-		await teamRef.update({slack_bot_access_token: authInfo.botAccessToken})
-		options.headers.Authorization = `Bearer ${authInfo.botAccessToken}`
-		res = await axios.get(url, data, options)
-	}
+	// 	let authTeam = null
+	// 	teamsRefs.docs.map(doc => doc.data()).filter(async team => {
+	// 		if (team.slack_bot_access_token == options.headers.Authorization.replace('Bearer ', ''))
+	// 			authTeam = team
+	// 	})
+	// 	authInfo = await getAuthInfo(authTeam.slack_auth_code)
+	// 	const teamRef = await firestore.collection('teams').doc(team.id).get()
+	// 	await teamRef.update({slack_bot_access_token: authInfo.botAccessToken})
+	// 	options.headers.Authorization = `Bearer ${authInfo.botAccessToken}`
+	// 	res = await axios.get(url, data, options)
+	// }
 	return res
 }
 
@@ -62,7 +69,6 @@ async function post(url, data, options) {
 async function getTeamInfo(accessToken) {
 	const res = await get('https://slack.com/api/team.info', {headers: {'Authorization': `Bearer ${accessToken}`}})
 	const data = res.data
-	console.log(data)
 	return { id: data.team.id, name: data.team.name}
 }
 
@@ -70,7 +76,6 @@ async function getTeamInfo(accessToken) {
 async function getUserInfo(userId, accessToken) {
 	const res = await get('https://slack.com/api/users.info', {params: {'user': userId}, headers: {'Authorization': `Bearer ${accessToken}`}})
 	const data = res.data
-	console.log(data)
 	return { id: data.user.id, name: data.user.real_name }
 }
 
@@ -78,14 +83,12 @@ async function getUserInfo(userId, accessToken) {
 async function openImChannel(userId, accessToken) {
 	const res = await get('https://slack.com/api/im.open', {params: {'user': userId}, headers: {'Authorization': `Bearer ${accessToken}`}})
 	const data = res.data
-	console.log(data)
 	return data.channel.id
 }
 
 
 async function sendMessage(data, accessToken) {
 	const res = await post('https://slack.com/api/chat.postMessage', data, { headers: { 'Authorization': `Bearer ${accessToken}` } })
-	console.log(res)
 	return res
 }
 
@@ -94,7 +97,6 @@ async function updateMessage(data, channel, ts, accessToken) {
 	data.channel = channel
 	data.ts = ts
 	let res = await post('https://slack.com/api/chat.update', data, { headers: { 'Authorization': `Bearer ${accessToken}` } })
-	console.log(res)
 	return res
 }
 
