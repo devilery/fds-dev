4c1daff1daee1dd1e3185d429ef790fe4b06d647
Tomas Ruzicka <me@tomasr.com>
2019-12-05 01:32:00 +0100

    delete commmand

--- src/api/test.js
+++ src/api/test.js
@@ -4,6 +4,49 @@ const { emmit } = require('../libs/event.js')
 const { retryBuild, jobDetails } = require('../libs/circleci')
 const { getPullRequestsForCommit } = require('../libs/github-api')
 const { trackEvent } = require('../libs/honeycomb')
+const { firestore } = require('../libs/firebase')
+
+
+function deleteCollection(db, collectionPath, batchSize) {
+  let collectionRef = db.collection(collectionPath);
+  let query = collectionRef.orderBy('__name__').limit(batchSize);
+
+  return new Promise((resolve, reject) => {
+    deleteQueryBatch(db, query, batchSize, resolve, reject);
+  });
+}
+
+function deleteQueryBatch(db, query, batchSize, resolve, reject) {
+  query.get()
+    .then((snapshot) => {
+      // When there are no documents left, we are done
+      if (snapshot.size == 0) {
+        return 0;
+      }
+
+      // Delete documents in a batch
+      let batch = db.batch();
+      snapshot.docs.forEach((doc) => {
+        batch.delete(doc.ref);
+      });
+
+      return batch.commit().then(() => {
+        return snapshot.size;
+      });
+    }).then((numDeleted) => {
+      if (numDeleted === 0) {
+        resolve();
+        return;
+      }
+
+      // Recurse on the next process tick, to avoid
+      // exploding the stack.
+      process.nextTick(() => {
+        deleteQueryBatch(db, query, batchSize, resolve, reject);
+      });
+    })
+    .catch(reject);
+}
 
 router.get('/', async(req, res) => {
 	// const { body } = req;
@@ -19,6 +62,21 @@ router.get('/', async(req, res) => {
 	console.log(output);
 
 	res.end();
+});
+
+router.get('/delete', async(req, res) => {
+
+	deleteCollection(firestore, 'teams', 100)
+	deleteCollection(firestore, 'repos', 100)
+	deleteCollection(firestore, 'gh_users', 100)
+	deleteCollection(firestore, 'checks', 100)
+	deleteCollection(firestore, 'commits', 100)
+	deleteCollection(firestore, 'github_owners', 100)
+	deleteCollection(firestore, 'pull_requests', 100)
+	deleteCollection(firestore, 'users', 100)
+
+	res.end()
+
 });
 
 module.exports = router;
