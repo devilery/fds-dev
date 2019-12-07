ac6e0ed36a934b9b50e35822b85dc16df8961c9e
Predrag Ristic <predyristic@gmail.com>
2019-12-07 20:41:49 +0100

    update

---
+++ src/entity/CommitCheck.ts
@@ -0,0 +1,32 @@
+import { BaseEntity, Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
+import Commit from './Commit';
+
+@Entity()
+export default class CommitCheck extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  githubId: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  status: 'pending' | 'success' | 'error' | 'failure'
+
+  @Column({nullable: true})
+  description: string;
+
+  @ManyToOne(type => Commit, commit => commit.checks)
+  commit: Commit;
+
+  @Column()
+  targetUrl: string;
+
+  @Column()
+  type: 'standard' | 'check'
+
+  @Column('jsonb')
+  rawData: any;
+}
--- src/entity/PullRequest.ts
+++ src/entity/PullRequest.ts
@@ -1,11 +1,39 @@
-import { BaseEntity, Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
+import { BaseEntity, Entity, Column, PrimaryGeneratedColumn, ManyToOne, ManyToMany } from "typeorm";
+import User from './User'
+import Commit from './Commit';
 
+@Entity()
 export default class PullRequest extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
+  remoteId: number;
+
+  @Column()
+  headSha: string;
+
+  @Column()
+  prNumber: number;
+
+  @Column()
+  slackThreadId: string;
+
+  @Column()
+  title: string;
+
+  @ManyToOne(type => User, { nullable: true })
+  user: User;
+
+  @Column()
+  websiteUrl: string;
+
+  @ManyToMany(type => Commit, commit => commit.pullRequests)
+  commits: Commit[]
+
+  @Column('jsonb')
+  rawData: any;
+
   @Column({default: 'github'})
   from: 'github';
-
-  
 }
