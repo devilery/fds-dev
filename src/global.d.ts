804b4b24d5fa5743d142d40b6e8436413a773f9f
Marek Vybiral <marekvybiral@email.cz>
2019-12-08 01:24:55 +0100

    changes

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,5 +4,8 @@ declare module 'axios-auth-refresh';
 declare namespace NodeJS {
   export interface ProcessEnv {
     GITHUB_PRIVATE_KEY: string;
+    SLACK_CLIENT_SECRET: string;
+    SLACK_CLIENT_ID: string;
+    SLACK_OAUTH_REDIRECT_URI: string;
   }
 }
--- src/subscribers/slack.ts
+++ src/subscribers/slack.ts
@@ -1,18 +1,36 @@
+// @ts-ignore
+import { strict as assert } from 'assert';
+
 const slack = require('../libs/slack-api.js')
 const { emmit } = require('../libs/event.js')
-import {Team, User} from '../entity'
+import { Team, User } from '../entity'
+import { WebClient } from '@slack/web-api'
+import { OauthAccessResult, UsersInfoResult, TeamInfoResult } from '../libs/slack-api'
+import { SlackUserAuthenticatedEventData } from '../api/slack-oauth-webhook'
 
 
-const authenticated = async function(data: any) {
-	const authInfo = await slack.getAuthInfo(data.code)
-	const teamInfo = await slack.getTeamInfo(authInfo.userAccessToken)
-	const userInfo = await slack.getUserInfo(authInfo.userId, authInfo.userAccessToken)
+const authenticated = async function(data: SlackUserAuthenticatedEventData) {
+	const authInfo = await (new WebClient()).oauth.access({
+		client_id: process.env.SLACK_CLIENT_ID,
+		client_secret: process.env.SLACK_CLIENT_SECRET,
+		redirect_uri: process.env.SLACK_OAUTH_REDIRECT_URI,
+		code: data.code
+	}) as OauthAccessResult;
+	assert(authInfo.ok, 'Fetching auth info failed!')
 
-	let team = await Team.findOne({where: { slackId: teamInfo.id }})
+	const client = new WebClient(authInfo.access_token)
+
+	const teamInfo = await client.team.info() as TeamInfoResult
+	assert(teamInfo.ok, 'Fetching team info failed!')
+
+	const userInfo = await client.users.info() as UsersInfoResult
+	assert(teamInfo.ok, 'Fetching team info failed!')
+
+	let team = await Team.findOne({where: { slackId: teamInfo }})
 	if (!team) {
 		team = Team.create({
-			slackId: teamInfo.id,
-			slackBotAccessToken: authInfo.botAccessToken
+			slackId: teamInfo.team.id,
+			slackBotAccessToken: authInfo.bot.bot_access_token
 		})
 
 		await team.save()
@@ -22,8 +40,8 @@ const authenticated = async function(data: any) {
 	let user = await User.findOne({where: { slackId: userInfo.id}})
 	if (!user) {
 		user = User.create({
-			slackId: userInfo.id,
-			name: userInfo.name,
+			slackId: userInfo.user.id,
+			name: userInfo.user.real_name,
 			team: team,
 			slackImChannelId: await slack.openImChannel(userInfo.id, authInfo.botAccessToken)
 		})
@@ -31,7 +49,6 @@ const authenticated = async function(data: any) {
 		await user.save()
 		emmit('user.created', user)
 	}
-
 }
 authenticated.eventType = 'slack.user.authenticated'
 
