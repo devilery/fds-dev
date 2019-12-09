7fac968476f69077d6289ecd6401bad7d3e0054e
Tomas Ruzicka <me@tomasr.com>
2019-12-09 12:25:00 +0100

    heroku pg

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -2,31 +2,38 @@ import {createConnection} from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import * as entities from '../entity'
 
+const config = {
+    'name': 'default',
+    'namingStrategy': new SnakeNamingStrategy(),
+    "type": 'postgres',
+    "synchronize": Boolean(process.env.PG_AUTO_DROP_DB),
+    "logging": process.env.TYPEORM_LOG == 'true',
+    "entities": Object.values(entities),
+    "migrations": [
+        "../migration/**/*.ts"
+    ],
+    "subscribers": [
+        "../subscriber/**/*.ts"
+    ],
+    "cli": {
+        "entitiesDir": "../entity",
+        "migrationsDir": "../migration",
+        "subscribersDir": "../subscriber"
+    }
+};
+
+if (process.env.DATABASE_URL) {
+    config['url'] = process.env.DATABASE_URL;
+} else {
+    config["host"] = process.env.TYPEORM_CONNECTION;
+    config["port"] = parseInt(process.env.PG_PORT);
+    config["username"] = process.env.PG_USERNAME;
+    config["password"] = process.env.PG_PASSWORD;
+    config["database"] = process.env.PG_DATABASE;
+}
+
 async function dbConnect() {
-    return createConnection({
-        'name': 'default',
-        'namingStrategy': new SnakeNamingStrategy(),
-        "type": 'postgres',
-        "host": process.env.TYPEORM_CONNECTION,
-        "port": parseInt(process.env.PG_PORT),
-        "username": process.env.PG_USERNAME,
-        "password": process.env.PG_PASSWORD,
-        "database": process.env.PG_DATABASE,
-        "synchronize": Boolean(process.env.PG_AUTO_DROP_DB),
-        "logging": process.env.TYPEORM_LOG == 'true',
-        "entities": Object.values(entities),
-        "migrations": [
-            "../migration/**/*.ts"
-        ],
-        "subscribers": [
-            "../subscriber/**/*.ts"
-        ],
-        "cli": {
-            "entitiesDir": "../entity",
-            "migrationsDir": "../migration",
-            "subscribersDir": "../subscriber"
-        }
-    })
+    return createConnection(config)
 }
 
 export default dbConnect
