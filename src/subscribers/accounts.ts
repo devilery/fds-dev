5143c035061236075857a8934e2f8c2f44ca21b6
Predrag Ristic <predyristic@gmail.com>
2020-02-01 17:43:06 +0100

    add welcome message

--- src/subscribers/accounts.ts
+++ src/subscribers/accounts.ts
@@ -1,6 +1,7 @@
 import { Team, User, ReviewInvite, PullRequest } from '../entity'
-import { getWelcomeMessage, getReviewRegisterMessage } from '../libs/slack-messages'
+import { getWelcomeMessage, getReviewRegisterMessage, getTutorialMessage } from '../libs/slack-messages'
 import { IRequestGithubReviewLogin } from '../events/types'
+import assert from '../libs/assert'
 
 
 const teamGhConnected = async function(team: Team) {
@@ -11,6 +12,17 @@ const teamGhConnected = async function(team: Team) {
 }
 teamGhConnected.eventType = 'team.gh.connected'
 
+
+const githubOAuthDone = async function(user_id: number) {
+  assert(!user_id, 'User id must be in event args...')
+
+  const user = await User.findOneOrFail(user_id);
+  const team = await user.relation('team');
+  const message = getTutorialMessage();
+  team.getSlackClient().chat.postMessage({ channel: user.slackImChannelId, blocks: message.blocks, text: message.text  })
+}
+
+githubOAuthDone.eventType = 'github.oauth.done'
 
 const userCreated = async function(user: User) {
   const message = getWelcomeMessage(user)
